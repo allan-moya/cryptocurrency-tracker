a2fff82f7877947f5769970d779c3afac03e0f41
allan-moya <allan.s.moya@gmail.com>
2021-02-18 08:05:46 -0800

    added live price and live chart

--- material-kit-master/index.js
+++ material-kit-master/index.js
@@ -1,4 +1,7 @@
 const form = document.querySelector('#searchForm');
+let LINEDATA = [];
+let data = [];
+let labels = [];
 form.addEventListener('submit', async function (e) {
     e.preventDefault();
     const res = await axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
@@ -6,3 +9,30 @@ form.addEventListener('submit', async function (e) {
     text.innerHTML = res.data.bpi.USD.rate;
     document.getElementById("coin").appendChild(text);
 })
+
+graph();
+setInterval("graph()", 30000);
+
+function graph() {
+    axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json?start=${moment(new Date()).subtract(1, 'month').format('YYYY-MM-DD')}&end=${moment(new Date()).format('YYYY-MM-DD')}`)
+        .then((response) => {
+            LINEDATA = { ...response.data.bpi };
+            data = Object.keys(LINEDATA).map(key => LINEDATA[key]);
+            labels = Object.keys(LINEDATA);
+            console.log(data);
+            console.log(labels);
+            new Chart(document.getElementById("chart"), {
+                type: 'line',
+                data: {
+                    labels: labels,
+                    datasets: [
+                        {
+                            label: 'Bitcoin',
+                            data: data,
+                            borderColor: "#3e95cd",
+                        }
+                    ]
+                }
+            });
+        });
+}
